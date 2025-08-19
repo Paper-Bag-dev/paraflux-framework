dc2b3ecd73d66d9c05ca217805d4a12602b9088d
Paper-Bag-dev <vikalpsh1234@gmail.com>
2025-08-19 16:10:28 +0530

    Added esbuild built 3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,7 +30,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.globalStore = void 0;
 const esbuild_1 = __importDefault(require("esbuild"));
 const createDevServer_1 = require("./devServer/createDevServer");
-const Store_1 = __importDefault(require("./store/Store"));
 const debouncer_1 = __importDefault(require("./utils/debouncer"));
 const Watcher_1 = require("./watcher/Watcher");
 const node_path_1 = __importDefault(require("node:path"));
@@ -30,8 +52,9 @@ async function buildApp() {
 async function main() {
     await buildApp();
     const { wss } = (0, createDevServer_1.createDevServer)(5000);
+    const globalStore = (await Promise.resolve().then(() => __importStar(require("./store/Store")))).default;
     const debouncedUpdate = (0, debouncer_1.default)(async (event, file) => {
-        await Store_1.default.updateRoot();
+        globalStore.updateRoot();
         console.log("Rebuilt Root!");
         wss.clients.forEach((client) => {
             if (client.readyState === 1) {
@@ -47,5 +70,5 @@ async function main() {
     });
 }
 main();
-var Store_2 = require("./store/Store");
-Object.defineProperty(exports, "globalStore", { enumerable: true, get: function () { return __importDefault(Store_2).default; } });
+var Store_1 = require("./store/Store");
+Object.defineProperty(exports, "globalStore", { enumerable: true, get: function () { return __importDefault(Store_1).default; } });
